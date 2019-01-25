eb8bb0ab7b89c3664fce6bf3a9f3b80e0dc0a885
Sumitkumar Rakshit <SumitR@talentica-all.com>
2019-01-25 18:22:27 +0530

    Updates

---
+++ precache-manifest.fb4369479be8cb5ef33ca12c63910e60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0986fe2feb42f6a21dec",
+    "url": "/shoppingCart-redux/static/js/runtime~main.0986fe2f.js"
+  },
+  {
+    "revision": "807bd8a3c8fea266fc6b",
+    "url": "/shoppingCart-redux/static/js/main.807bd8a3.chunk.js"
+  },
+  {
+    "revision": "f0f126ae481c68c49f20",
+    "url": "/shoppingCart-redux/static/js/1.f0f126ae.chunk.js"
+  },
+  {
+    "revision": "807bd8a3c8fea266fc6b",
+    "url": "/shoppingCart-redux/static/css/main.739b6d12.chunk.css"
+  },
+  {
+    "revision": "f22bcee487c265af2ac894e91e96947f",
+    "url": "/shoppingCart-redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shopping-cart/precache-manifest.d07b81e7ddc91e388681182e38f6a607.js"
+  "/shoppingCart-redux/precache-manifest.fb4369479be8cb5ef33ca12c63910e60.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shopping-cart/index.html", {
+workbox.routing.registerNavigationRoute("/shoppingCart-redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
