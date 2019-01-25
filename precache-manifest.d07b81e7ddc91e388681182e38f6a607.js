3b0b6cf5c725e936ecdb706f4cb59be1beaa43f4
Sumitkumar Rakshit <SumitR@talentica-all.com>
2019-01-25 18:17:04 +0530

    Updates

---
+++ precache-manifest.d07b81e7ddc91e388681182e38f6a607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b509cb4f645738d95a2a",
+    "url": "/shopping-cart/static/js/runtime~main.b509cb4f.js"
+  },
+  {
+    "revision": "d6ec70a43acd258c42aa",
+    "url": "/shopping-cart/static/js/main.d6ec70a4.chunk.js"
+  },
+  {
+    "revision": "f0f126ae481c68c49f20",
+    "url": "/shopping-cart/static/js/1.f0f126ae.chunk.js"
+  },
+  {
+    "revision": "d6ec70a43acd258c42aa",
+    "url": "/shopping-cart/static/css/main.739b6d12.chunk.css"
+  },
+  {
+    "revision": "cb4b8a52904bfcd026207807a8491099",
+    "url": "/shopping-cart/index.html"
+  }
+];
